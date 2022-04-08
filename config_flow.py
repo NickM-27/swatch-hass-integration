23f8c07b244b0056419622bc2a8009fff0111c4d
Nick Mowen <nickmowen213@gmail.com>
2022-04-08 07:58:13 -0600

    Add logging

--- config_flow.py
+++ config_flow.py
@@ -36,6 +36,7 @@ def get_config_entry_title(url_str: str) -> str:
 
 
 def validate_host(host) -> bool:
+    _LOGGER.error(f"Getting the swatch host {host}")
     resp = requests.get(host)
 
     if not resp or resp.status_code != 200:
