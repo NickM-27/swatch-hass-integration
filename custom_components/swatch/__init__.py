8888d1119aae951900f38fbe553f5da82239d1eb
Nicolas Mowen <nickmowen213@gmail.com>
2023-05-13 06:51:32 -0600

    Fix update issues

--- custom_components/swatch/__init__.py
+++ custom_components/swatch/__init__.py
@@ -116,7 +116,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         ATTR_MODEL: model,
     }
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups (entry, PLATFORMS)
 
     return True
 
